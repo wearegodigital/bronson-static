0a0f271852465a60e07fe8946badd587d905c0f3
sean-wilk <sean-wilk@users.noreply.github.com>
2022-11-25 15:21:49 +0000

    deploy: 6f91b886b08549b7350c5f945aaea36afbbf5402

---
+++ assets/js/script.js
@@ -0,0 +1,88 @@
+gsap.registerPlugin(ScrollTrigger);
+
+$('document').ready( function(){
+  
+  // Setting speed (duration) variables
+  var tl1speed = 110;
+  var tl2speed = 115;
+  var tl3speed = 110;
+  var slowScale = 0.25;
+  var normalScale = 1;
+  
+  // Adding duplicates of items to make transition smoother
+  $('#cloned').append( $('#cloned .item').clone() );
+  
+  // Setting Timelines
+  tl1 = new TimelineMax({repeat: -1});
+  tl1.to( $('#cloned .item:nth-child(3n+1)'), tl1speed, { x:-($('#cloned').width()), ease: Power0.easeNone });
+  
+  tl2 = new TimelineMax({repeat: -1});
+  tl2.to( $('#cloned .item:nth-child(3n+2)'), tl2speed, { x:-($('#cloned').width()), ease: Power0.easeNone });
+  
+  tl3 = new TimelineMax({repeat: -1});
+  tl3.to( $('#cloned .item:nth-child(3n)'), tl3speed, { x:-($('#cloned').width()), ease: Power0.easeNone });
+  
+  // Setting timescale to slow on load
+  var slowFlag = 0;
+  TweenLite.to(tl1, 1, {timeScale:slowScale, ease:Quad.easeOut});
+  TweenLite.to(tl2, 1, {timeScale:slowScale, ease:Quad.easeOut});
+  TweenLite.to(tl3, 1, {timeScale:slowScale, ease:Quad.easeOut});
+  slowFlag = 1;
+  
+  // Speeding up after mouse has been stationary for 2 seconds and hiding logo
+  var timer;
+  timer = setTimeout(function() {
+    TweenLite.to(tl1, normalScale, {timeScale:1, ease:Quad.easeOut});
+    TweenLite.to(tl2, normalScale, {timeScale:1, ease:Quad.easeOut});
+    TweenLite.to(tl3, normalScale, {timeScale:1, ease:Quad.easeOut});
+    $(".page-interface-element, .logo-container").fadeOut(1000);
+    slowFlag = 0;
+  }, 4000);
+  
+  $(document).on('mousemove', function(e){
+     clearTimeout(timer);
+     if(slowFlag != 1){
+      TweenLite.to(tl1, 1, {timeScale:slowScale, ease:Quad.easeOut});
+      TweenLite.to(tl2, 1, {timeScale:slowScale, ease:Quad.easeOut});
+      TweenLite.to(tl3, 1, {timeScale:slowScale, ease:Quad.easeOut});
+      $(".page-interface-element, .logo-container").fadeIn(1000);
+      slowFlag = 1;
+     }
+
+     timer = setTimeout(function() {
+       TweenLite.to(tl1, normalScale, {timeScale:1, ease:Quad.easeOut});
+       TweenLite.to(tl2, normalScale, {timeScale:1, ease:Quad.easeOut});
+       TweenLite.to(tl3, normalScale, {timeScale:1, ease:Quad.easeOut});
+       $(".page-interface-element, .logo-container").fadeOut(1000);
+       slowFlag = 0;
+     }, 4000);
+  });
+  
+});
+
+function mouseMoveFunc(evt) {
+  const maxY1 = gsap.getProperty(".items", "height") * -0.125;
+  const maxY2 = gsap.getProperty(".items", "height") * -0.275;
+  const maxY3 = gsap.getProperty(".items", "height") * -0.05;
+  
+  const percent = gsap.utils.normalize(0, innerHeight, evt.pageY);
+  
+  gsap.to("#cloned .item:nth-child(3n+1)", {
+    duration: 0.2,
+    y: percent * maxY1 - maxY1 / 2
+  });
+  
+  gsap.to("#cloned .item:nth-child(3n+2)", {
+    duration: 0.2,
+    y: percent * maxY2 - maxY2 / 2
+  });
+  
+  gsap.to("#cloned .item:nth-child(3n)", {
+    duration: 0.2,
+    y: percent * maxY3 - maxY3 / 2
+  });
+}
+
+window.addEventListener("mousemove", mouseMoveFunc);
+
+
